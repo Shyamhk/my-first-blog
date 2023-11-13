40434cf78013ee8daad1db965aee16cf4d0784d2
Shyam <https://Shyam@github.com>
2023-11-13 11:08:53 +0000

    sett

--- Assignment project/djangogirls/mysite/settings.py
+++ Assignment project/djangogirls/mysite/settings.py
@@ -124,3 +124,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = Path(BASE_DIR / 'static')
+MEDIA_ROOT = Path(BASE_DIR / 'media')
