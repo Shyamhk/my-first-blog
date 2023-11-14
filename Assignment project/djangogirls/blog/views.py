ea149a277c036993061c9ad416ab93bca8173e7f
Shyam <shyamsundersaini654@gmail.com>
2023-11-14 14:42:26 +0530

    updates on 14 nov

--- Assignment project/djangogirls/blog/views.py
+++ Assignment project/djangogirls/blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from django.utils import timezone
+from .models import Post
 # Create your views here.
-def index(request):
-    return render(request,"index.html")
+def post_list(request):
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    return render(request,"index.html",{'posts':posts})
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request,"post_details.html",{'post':post})
